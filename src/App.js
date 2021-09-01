f64c83cf1beb12dd7cbb823412101127a0fb94f5
marmor32 <bhmi1994@gmail.com>
2021-09-01 09:42:19 +0300

    add redux state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    case 'ZERO':
+      return 0
+    default:
+      return state
+  }
+}
+const store = createStore(counterReducer)
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById('root'))
+}
+
 function App() {
   return (
-    <div className="App">
-      <h1>test redux app</h1>
-    </div>
+    <div>
+          <div>
+            {store.getState()}
+          </div>
+          <button 
+            onClick={e => store.dispatch({ type: 'INCREMENT' })}
+          >
+            plus
+          </button>
+          <button
+            onClick={e => store.dispatch({ type: 'DECREMENT' })}
+          >
+            minus
+          </button>
+          <button 
+            onClick={e => store.dispatch({ type: 'ZERO' })}
+          >
+            zero
+          </button>
+        </div>
   );
 }
 
